e653c79d36966d0d6b2425ed36528724ff19ba38
kenocvr <kenocvr@gmail.com>
2017-11-04 12:10:43 -0700

    added hero abilities model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ export class Character {
   name: string;
   type: string;  // type: tank, healer, dps, etc
   abilities: any;
+  damage: string;
   image: string;
 }
 
@@ -23,69 +24,61 @@ const CHARACTERS: Character[] = [
     name: 'Mercy',
     type: 'Healer',
     abilities: ['heal', 'damage boost'],
+    damage: '99',
     image: "https://i.pinimg.com/474x/a5/02/4e/a5024e9bef07692573fa67b7e8834586--overwatch-mercy-character-names.jpg"
   },
   {
     name: 'Genji',
     type: 'Dps',
     abilities: ['heal', 'damage boost'],
+    damage: '99',
     image: "https://www.pcinvasion.com/wp-content/uploads/2016/06/genji-profile-icon.png"
   },
   {
     name: 'Lucio',
     type: 'Healer',
-    abilities: ['heal', 'damage boost'],
+    abilities: [
+
+        {
+          abName: 'Wall Ride',
+          description: `Can ride along a wall
+          Jump onto a wall to ride along it.`
+        }
+      ,
+
+        {
+          abName: 'Healing Boost',
+          description: `Heals nearby teammates.`
+        }
+
+    ],
+    damage: '99',
     image: "http://www.lastpick.net/assets/heroes/Lucio.png"
   },
   {
     name: 'Bastion',
     type: 'Dps',
     abilities: ['heal', 'damage boost'],
-    image: "http://www.lastpick.net/assets/heroes/Lucio.png"
+    damage: '99',
+    image: "http://www.lastpick.net/assets/heroes/Bastion.png"
   },
   {
     name: 'Ana',
     type: 'Healer',
     abilities: ['heal', 'damage boost'],
-    image: "http://www.lastpick.net/assets/heroes/Lucio.png"
+    damage: '99',
+    image: "http://www.lastpick.net/assets/heroes/Ana.png"
   },
   {
     name: 'Zarya',
     type: 'Tank',
     abilities: ['heal', 'damage boost'],
-    image: "http://www.lastpick.net/assets/heroes/Lucio.png"
-  },
-  {
-    name: 'Lucio',
-    type: 'Healer',
-    abilities: ['heal', 'damage boost'],
-    image: "http://www.lastpick.net/assets/heroes/Lucio.png"
+    damage: '99',
+    image: "http://www.lastpick.net/assets/heroes/Zarya.png"
   }
 ];
 
-const CHARACTERSSTATS: CharacterStats[] = [
-{
-    name: 'Lucio',
-    damage: '16',
-    abilities: [
-      {
-        wallRide:
-        {
-          abName: 'Wall Ride',
-          description: `Can ride along a wall
-          Jump onto a wall to ride along it.`
-        }
-      },
-      {
-        heal:
-        {
-          abName: 'Healing Boost',
-          description: `Heals nearby teammates.`
-        }
-      }
-    ]
-  }
-];
+
 
 @Component({
   selector: 'app-root',
@@ -94,13 +87,13 @@ const CHARACTERSSTATS: CharacterStats[] = [
 })
 export class AppComponent {
   characters = CHARACTERS;
-  charactersstats = CHARACTERSSTATS;
+
 
   selectedCharacter: Character;
-  selectedCharacterStats: CharacterStats
+
 
   onSelect(character: Character): void {
     this.selectedCharacter = character;
-    //this.selectedCharacterStats = characterstats;
+
   }
 }
